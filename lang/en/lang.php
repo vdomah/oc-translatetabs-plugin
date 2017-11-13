810710683e684ad37565dede963347b401f966bc
Vdomah <firebatt@ukr.net>
2017-11-13 09:49:58 +0200

    Plugin description

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,6 +1,6 @@
 <?php return [
     'plugin' => [
         'name' => 'Translate Tabs',
-        'description' => ''
+        'description' => 'Adds possibility to display translatable fields grouped into tabs by locales'
     ]
 ];
